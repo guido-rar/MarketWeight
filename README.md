8afe2c96658d79731fb5103cc6849e5e359c1d60
carlete-afk <carlos.belloet12de1@gmail.com>
2024-08-09 09:12:46 -0300

    Primer Commit

---
+++ README.md
@@ -0,0 +1,55 @@
+<h1 align="center">E.T. Nº12 D.E. 1º "Libertador Gral. José de San Martín"</h1>
+<p align="center">
+  <img src="https://et12.edu.ar/imgs/computacion/vamoaprogramabanner.png" alt="Banner Computación">
+</p>
+
+## Computación 2023
+
+**Asignatura**: <!-- REEMPLAZA este comentario por el nombre de la asignatura -->
+
+**Nombre TP**: <!-- REEMPLAZA este comentario por el nombre del TP -->
+
+**Apellido y nombre Alumno**: <!-- REEMPLAZA este comentario por tu apellido y nombre -->
+
+**Curso**: <!-- REEMPLAZA este comentario por tu curso -->
+
+# Título del Proyecto
+
+_Acá va un párrafo que describa lo que es el proyecto._
+
+## Comenzando 🚀
+
+Clonar el repositorio github, desde Github Desktop o ejecutar en la terminal o CMD:
+<!-- cambia el link de abajo al de tu repositorio y BORRA ESTE COMENTARIO -->
+```
+git clone https://github.com/ET12DE1Computacion/simpleTemplateCSharp
+```
+
+### Pre-requisitos 📋
+
+- .NET 6.0.5 (SDK .NET 6.0.300) - [Descargar](https://dotnet.microsoft.com/download/dotnet/6.0)
+
+## Despliegue 📦
+
+_Agrega notas adicionales sobre que cosas se debe instalar, configurar y como hacer deploy_
+
+## Construido con 🛠️
+
+_Menciona las herramientas y versiones que utilizaste para crear tu proyecto_
+
+* [Visual Studio Code](https://code.visualstudio.com/#alt-downloads) - Editor de código.
+
+## Versionado 📌
+
+Usamos [SemVer](http://semver.org/) para el versionado. Para todas las versiones disponibles, mira los [tags en este repositorio](https://github.com/ET12DE1Computacion/simpleTemplateCSharp/tags).
+
+## Autores ✒️
+
+_Menciona a todos aquellos que ayudaron a levantar el proyecto desde sus inicios_
+
+* **Max Power** - *Desarrollo* - [Maxpower](https://github.com/maxpower)
+* **Cosme Fulanito** - *Documentación* - [Cosmefulanito](#Cosmefulanito)
+
+## Licencia 📄
+
+Este proyecto está bajo la Licencia (Tu Licencia) - mira el archivo [LICENSE.md](LICENSE.md) para detalles.
---
+++ scripts sql/trinance-ddl.sql
@@ -0,0 +1,110 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema 5to_Trinance
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `5to_Trinance` ;
+
+-- -----------------------------------------------------
+-- Schema 5to_Trinance
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `5to_Trinance` ;
+USE `5to_Trinance` ;
+
+-- -----------------------------------------------------
+-- Table `Usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Usuario` (
+  `idUsuario` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellido` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `pass` CHAR(64) NOT NULL,
+  `Saldo` FLOAT NOT NULL,
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE INDEX `idUsuario_UNIQUE` (`idUsuario` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Moneda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Moneda` (
+  `idMoneda` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `precio` DECIMAL NOT NULL,
+  `cantidad` INT NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idMoneda`),
+  UNIQUE INDEX `idMoneda_UNIQUE` (`idMoneda` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `UsuarioMoneda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `UsuarioMoneda` (
+  `idUsuario` INT UNSIGNED NOT NULL,
+  `idMoneda` INT UNSIGNED NOT NULL,
+  `cantidad` FLOAT NOT NULL,
+  PRIMARY KEY (`idUsuario`, `idMoneda`),
+  INDEX `fk_BilleteraMoneda_Moneda1_idx` (`idMoneda` ASC) VISIBLE,
+  INDEX `fk_BilleteraMoneda_Usuario1_idx` (`idUsuario` ASC) VISIBLE,
+  CONSTRAINT `fk_BilleteraMoneda_Moneda1`
+    FOREIGN KEY (`idMoneda`)
+    REFERENCES `Moneda` (`idMoneda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_BilleteraMoneda_Usuario1`
+    FOREIGN KEY (`idUsuario`)
+    REFERENCES `Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Historial`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Historial` (
+  `idMoneda` INT UNSIGNED NOT NULL,
+  `cantidad` FLOAT UNSIGNED NULL,
+  `fechaHora` DATETIME NOT NULL,
+  `accion` TINYINT UNSIGNED NOT NULL,
+  `idUsuario` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`idMoneda`, `idUsuario`),
+  INDEX `fk_Historial_Moneda1_idx` (`idMoneda` ASC) VISIBLE,
+  UNIQUE INDEX `idMoneda_UNIQUE` (`idMoneda` ASC) VISIBLE,
+  INDEX `fk_Historial_Usuario1_idx` (`idUsuario` ASC) VISIBLE,
+  CONSTRAINT `fk_Historial_Moneda1`
+    FOREIGN KEY (`idMoneda`)
+    REFERENCES `Moneda` (`idMoneda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Historial_Usuario1`
+    FOREIGN KEY (`idUsuario`)
+    REFERENCES `Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `5to_Trinance`;
+
+DELIMITER $$
+USE `5to_Trinance`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `5to_Trinance`.`Usuario_BEFORE_INSERT` BEFORE INSERT ON `Usuario` FOR EACH ROW
+BEGIN
+
+END
+$$
+
+
+DELIMITER ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
