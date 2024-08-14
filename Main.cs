9d9594e02387b1911d6e747867dc140dd7f5c609
FranciscoBHF <114776184+FranciscoBHF@users.noreply.github.com>
2024-08-14 14:30:20 -0300

    correccion de fecha, bacndera de limpieza

---
+++ Main.cs
@@ -0,0 +1,17 @@
+class MarketWeight
+{
+    internal static bool salir = false;
+}
+
+class Program
+{
+    public static void Main() {
+
+        do {
+
+            Menu.ImprimirPantallaPrincipal();
+
+        } while ( MarketWeight.salir == false );
+    
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Runtime.CompilerServices;
+
+class Menu
+{
+    private static ConsoleKey _tecla;
+    private static int _opcionActual = 0;
+    private static string[] _opciones = ["Iniciar sesión", "Registrarse", "Mostrar Lista de Monedas", "Salir"];
+    internal static string Centrar(string text)
+    {
+        string[] lines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+        int longestLength = lines.Max(line => line.Length);
+
+        string spaces = new(' ', (Console.WindowWidth - longestLength) / 2);
+
+        string centeredText = string.Join(Environment.NewLine, lines.Select(line => spaces + line));
+
+        return centeredText;
+    }
+
+    internal static void ReiniciarColores()
+    {
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    internal static ConsoleKey EscucharTeclado()
+    {
+
+        _tecla = Console.ReadKey(true).Key;
+
+        return _tecla;
+    }
+
+    internal static void ImprimirMenu(string[] opciones)
+    {
+        for (int i = 0; i < opciones.Length; i++)
+        {
+            if (_opcionActual == i)
+            {
+                ReiniciarColores();
+                Console.WriteLine(Centrar($"> {opciones[i]} <"));
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine(Centrar(opciones[i]));
+            }
+        }
+
+        ReiniciarColores();
+        
+    }
+
+    internal static void ImprimirPantallaPrincipal()
+    {
+        ReiniciarColores();
+        Console.Clear();
+
+        Console.WriteLine("\n\n\n");
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(Centrar(ASCII.a));
+
+        Console.WriteLine("\n");
+
+        ReiniciarColores();
+
+        ImprimirMenu(_opciones);
+
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+
+        Console.WriteLine(ASCII.Creditos);
+
+        System.Console.WriteLine(_opcionActual);
+
+        EscucharTeclado();
+
+        if (_tecla == ConsoleKey.DownArrow && _opcionActual < _opciones.Length -1)
+        {
+            _opcionActual++;
+            ImprimirPantallaPrincipal();
+
+        }
+        
+        else if (_tecla == ConsoleKey.DownArrow && _opcionActual >= _opciones.Length -1)
+            _opcionActual = 0;
+
+        if (_tecla == ConsoleKey.UpArrow && _opcionActual > 0)
+        {
+            _opcionActual--;
+            ImprimirPantallaPrincipal();
+
+        }
+
+        else if (_tecla == ConsoleKey.UpArrow && _opcionActual <= 0)
+            _opcionActual = _opciones.Length -1;
+        
+    }
+}
