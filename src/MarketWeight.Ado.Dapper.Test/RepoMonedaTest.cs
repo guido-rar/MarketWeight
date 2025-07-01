29a14c667c076717b766e4bb7536bb7750d77da8
guido-rar <guido.gavilanet12de1@gmail.com>
2025-07-01 08:51:40 -0300

    Test1.00

--- src/MarketWeight.Ado.Dapper.Test/RepoMonedaTest.cs
+++ src/MarketWeight.Ado.Dapper.Test/RepoMonedaTest.cs
@@ -74,4 +74,70 @@ public class RepoMonedaTest : TestBase
 
         Assert.NotEmpty(monedas);
     }
+    /*Async*/
+    [Fact]
+
+    public async Task CrearMonedaOKAsync()
+    {
+        Moneda monedaPepe = new Moneda
+        {
+            Precio = 10m,
+            Cantidad = 2m,
+            Nombre = "pepe"
+        };
+
+        Moneda monedaVirgo = new Moneda
+        {
+            Precio = 300m,
+            Cantidad = 5000m,
+            Nombre = "VirgoCoin"
+        };
+
+        await _repo.Alta(monedaPepe);
+        await _repo.Alta(monedaVirgo);
+        
+    }
+
+    [Fact]
+    public async Task CrearMonedaFailAsync()
+    {
+        Moneda monedaDoge = new Moneda
+        {
+            Precio = 77m,
+            Cantidad = 100m,
+            Nombre = "DogeCoin"
+        };
+
+        Moneda monedaLitecoin = new Moneda
+        {
+            Precio = 77m,
+            Cantidad = 100m,
+            Nombre = "Litecoin"
+        };
+
+        var error =  Assert.ThrowsAsync<MySqlException> (()=> await _repo.Alta(monedaDoge));
+        Assert.Equal("Moneda ya registrada :v", error.Message);
+
+        error =  Assert.ThrowsAsync<MySqlException> (()=>await _repo.Alta(monedaLitecoin));
+        Assert.Equal("Moneda ya registrada :v", error.Message);
+    }
+
+    [Fact]
+    public async Task TraerOKAsync()
+    {
+        var monedas = await _repo.Obtener();
+        
+        Assert.NotEmpty(monedas);
+        Assert.Contains(monedas,
+            m => m.Nombre == "Bitcoin" || m.Nombre == "pepe"  || m.Nombre == "dogeCoin" || m.Nombre == "VirgoCoin");
+    }
+    
+    [Fact]
+    public void ObtenerConCondicionOKAsync()
+    {
+        var monedas = await +_repo.ObtenerConCondicion("precio >= 100");
+
+        Assert.NotEmpty(monedas);
+    }
+
 }
